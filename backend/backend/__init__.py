22f91e53bcacf2690bd3743f39b349ca77fbc26d
chacychavez <cyrus@insynchq.com>
2022-05-22 13:36:23 +0800

    Update creating and running app sever

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -5,11 +5,21 @@ from flask_restful import Api
 from backend.models import db
 from backend.resources.users import Users
 
-app = Flask(__name__)
-app.config.from_object("backend.config")
-api = Api(app)
-db.init_app(app)
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("backend.config")
+    api = Api(app)
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    # Add resources
+    api.add_resource(Users, '/', '/users')
+
+    return app
 
 
-# Add resources
-api.add_resource(Users, '/', '/users')
+if __name__ == "__main__":
+    create_app().run()
