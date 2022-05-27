0fbd188805dca1bd6c78f032982db8e622fd5dd4
chacychavez <cyrus@insynchq.com>
2022-05-27 21:07:27 +0800

    Add initial test for app/api

--- backend/tests/test_backend.py
+++ backend/tests/test_backend.py
@@ -1,5 +1,24 @@
-from backend import __version__
+import pytest
+
+from backend import __version__, create_app
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({"TESTING": True})
+    yield app
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
